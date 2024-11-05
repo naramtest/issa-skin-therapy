7d5b5217126dbd573248b66165d54bdc498f35c9
naram <naramalkoht@gmail.com>
2024-11-05 13:21:35 +0300

    video section

---
+++ resources/views/components/product/section/video.blade.php
@@ -0,0 +1,22 @@
+<x-home.section-container
+    class="card-overlay relative mt-10 flex h-[600px] items-center justify-center overflow-hidden"
+>
+    <video
+        class="absolute inset-0 h-full w-full object-cover"
+        autoplay
+        muted
+        playsinline
+        webkit-playsinline
+        loop
+        preload="auto"
+        src="{{ asset("storage/video/patent-delivery-technology.webm") }}"
+    ></video>
+    <div class="z-[10] flex w-[60%] flex-col items-center text-white">
+        <p class="text-[13px] font-[200] uppercase tracking-[2px]">
+            Made in USA
+        </p>
+        <h2 class="headline-font mt-4 text-center">
+            Patent Delivery Technology
+        </h2>
+    </div>
+</x-home.section-container>
--- resources/views/storefront/product/show.blade.php
+++ resources/views/storefront/product/show.blade.php
@@ -1,5 +1,7 @@
 <x-store-main-layout>
     <main>
         <x-product.section.detials />
+
+        <x-product.section.video />
     </main>
 </x-store-main-layout>
