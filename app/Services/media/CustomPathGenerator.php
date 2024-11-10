1ef3ffb9b428b10a308b00d2a0ca12760cf15a7a
naram <naramalkoht@gmail.com>
2024-11-10 17:54:10 +0300

    fix media folder

---
+++ app/Services/media/CustomPathGenerator.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Services\media;
+
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Spatie\MediaLibrary\Support\PathGenerator\PathGenerator;
+
+class CustomPathGenerator implements PathGenerator
+{
+    public function getPathForConversions(Media $media): string
+    {
+        return $this->getPath($media) . "conversions/";
+    }
+
+    public function getPath(Media $media): string
+    {
+        return "media/" .
+            $media->created_at->format("Y/m") .
+            "/" .
+            $media->id .
+            "/";
+    }
+
+    public function getPathForResponsiveImages(Media $media): string
+    {
+        return $this->getPath($media) . "responsive-images/";
+    }
+}
