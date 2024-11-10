28f62d02375927dedbe4473d9884de19cc827f81
naram <naramalkoht@gmail.com>
2024-11-10 11:45:00 +0300

    add filament shield

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -38,6 +39,7 @@ class AdminPanelProvider extends PanelProvider
                     "ar",
                 ]),
                 FilamentChainedTranslationManagerPlugin::make(),
+                FilamentShieldPlugin::make(),
             ])
             ->discoverResources(
                 in: app_path("Filament/Resources"),
