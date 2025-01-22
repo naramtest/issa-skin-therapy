23f585464b5bc23c95367487f681ab8fc0bae269
naram <naramalkoht@gmail.com>
2025-01-22 09:05:32 +0300

    category and contact us  responsive

--- resources/views/storefront/contact.blade.php
+++ resources/views/storefront/contact.blade.php
@@ -1,10 +1,13 @@
 <x-store-main-layout>
-    <div class="flex gap-8 padding-from-side-menu pt-24">
-        <div class="w-[50%]">
-            <h1 class="text-[95px] font-[800]">Contact Us
+    <div
+        class="padding-from-side-menu flex flex-col gap-8 pt-12 md:flex-row lg:pt-24"
+    >
+        <div class="md:w-[50%]">
+            <h1 class="text-4xl font-[800] lg:text-[95px]">
+                {{ __("store.Contact Us") }}
             </h1>
-            <form>
-                <div class="mb-6 grid grid-cols-2 gap-4">
+            <form class="mt-10 md:mt-12">
+                <div class="mb-6 grid grid-cols-1 gap-4 md:grid-cols-2">
                     <x-checkout.input-field
                         label="{{ __('store.Name') }}:"
                         :required="true"
@@ -20,7 +23,7 @@
                         type="email"
                     />
                 </div>
-                <div class="mb-6 grid grid-cols-2 gap-4">
+                <div class="mb-6 grid grid-cols-1 gap-4 md:grid-cols-2">
                     <x-checkout.input-field
                         label="{{ __('dashboard.Phone number') }}:"
                         :required="true"
@@ -39,50 +42,61 @@
                     label="{{ __('store.Message') }}:"
                     :required="true"
                     placeHolder="{{ __('dashboard.Type your message') }}"
-                    field="message" />
-                <button
-                    type="submit"
-                    class="w-full mt-6"
-
-                >
+                    field="message"
+                />
+                <button type="submit" class="mt-6 w-full">
                     <x-general.button-black-animation>
-                         <span class="relative z-10 inline-block ">
-                                    {{ __("store.Shop Now") }}
-                         </span>
+                        <span class="relative z-10 inline-block">
+                            {{ __("store.Shop Now") }}
+                        </span>
                     </x-general.button-black-animation>
                 </button>
             </form>
         </div>
-        <div class=" w-[50%]
-                ">
-            <iframe loading="lazy"
-                    class="w-full  h-[650px] rounded-xl"
-                    src="https://maps.google.com/maps?q=West%20Chester%2C%20Pennsylvania&amp;t=m&amp;z=10&amp;output=embed&amp;iwloc=near"
-                    title="West Chester, Pennsylvania" aria-label="West Chester, Pennsylvania"></iframe>
+        <div class="md:w-[50%]">
+            <iframe
+                loading="lazy"
+                class="h-[650px] w-full rounded-xl"
+                src="https://maps.google.com/maps?q=West%20Chester%2C%20Pennsylvania&amp;t=m&amp;z=10&amp;output=embed&amp;iwloc=near"
+                title="West Chester, Pennsylvania"
+                aria-label="West Chester, Pennsylvania"
+            ></iframe>
         </div>
     </div>
-    <div class="grid grid-cols-4 padding-from-side-menu pb-24 pt-12 gap-4">
-        <div class="border-gray-200 border rounded-xl p-7">
-            <h3 class="text-[#B1B1B1] font-medium text-xs">{{ __('dashboard.info.address') }}</h3>
-            <p class="text-sm  mt-3 font-medium">{{$info->address}}</p>
+    <div
+        class="padding-from-side-menu grid grid-cols-1 gap-4 pb-24 pt-12 md:grid-cols-2 lg:grid-cols-4"
+    >
+        <div class="rounded-xl border border-gray-200 p-7">
+            <h3 class="text-xs font-medium text-[#B1B1B1]">
+                {{ __("dashboard.info.address") }}
+            </h3>
+            <p class="mt-3 text-sm font-medium">{{ $info->address }}</p>
         </div>
-        @if(count($info->email))
-            <div class="border-gray-200 border rounded-xl p-7">
-                <h3 class="text-[#B1B1B1] font-medium text-xs">{{ __('dashboard.EMAIL') }}</h3>
-                <p class="text-sm  mt-3 font-medium">{{$info->email[0]['email']}}</p>
-            </div>
-        @endif
-        @if(count($info->phone))
-
-            <div class="border-gray-200 border rounded-xl p-7">
-                <h3 class="text-[#B1B1B1] font-medium text-xs">{{ __('dashboard.WHATSAPP ONLY') }}</h3>
-                <p class="text-sm  mt-3 font-medium">{{$info->phone[0]['number']}}</p>
+        @if (count($info->email))
+            <div class="rounded-xl border border-gray-200 p-7">
+                <h3 class="text-xs font-medium text-[#B1B1B1]">
+                    {{ __("dashboard.EMAIL") }}
+                </h3>
+                <p class="mt-3 text-sm font-medium">
+                    {{ $info->email[0]["email"] }}
+                </p>
             </div>
         @endif
 
-        <div class="border-gray-200 border rounded-xl p-7">
-            <h3 class="text-[#B1B1B1] font-medium text-xs">FOLLOW US</h3>
-            <ul class="mt-3 flex w-2/3  items-center justify-between">
+        @if (count($info->phone))
+            <div class="rounded-xl border border-gray-200 p-7">
+                <h3 class="text-xs font-medium text-[#B1B1B1]">
+                    {{ __("dashboard.WHATSAPP ONLY") }}
+                </h3>
+                <p class="mt-3 text-sm font-medium">
+                    {{ $info->phone[0]["number"] }}
+                </p>
+            </div>
+        @endif
+
+        <div class="rounded-xl border border-gray-200 p-7">
+            <h3 class="text-xs font-medium text-[#B1B1B1]">FOLLOW US</h3>
+            <ul class="mt-3 flex w-2/3 items-center justify-between">
                 @foreach ($info->social as $social)
                     <x-share.icon
                         class="h-6 w-6"
--- resources/views/storefront/product/category.blade.php
+++ resources/views/storefront/product/category.blade.php
@@ -2,7 +2,9 @@
     <main class="relative">
         <x-shop.section.hero label="{{$collectionType->name}}" />
         <section class="padding-from-side-menu py-12">
-            <div class="mt-8 grid grid-cols-4 gap-6">
+            <div
+                class="mt-8 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4"
+            >
                 @foreach ($products as $product)
                     <x-general.product-item-card :product="$product" />
                 @endforeach
