f7a386d23045ff497b7a9c00d6ed9ea9f9f2b4f0
naram <naramalkoht@gmail.com>
2025-02-27 16:13:32 +0300

    fix city auto select

--- resources/views/components/checkout/sections/payment-methods-component.blade.php
+++ resources/views/components/checkout/sections/payment-methods-component.blade.php
@@ -36,70 +36,68 @@
 
                 {{--  --}}
                 {{-- Tabby Payment Method --}}
-                @if (App::isLocal())
-                    <div>
-                        @if ($isAvailable)
-                            <div class="relative flex gap-x-4 pb-4">
-                                <div class="flex h-6 items-center">
-                                    <input
-                                        id="tabby"
-                                        name="payment_method"
-                                        type="radio"
-                                        wire:model.live="form.payment_method"
-                                        value="{{ \App\Enums\Checkout\PaymentMethod::TABBY->value }}"
-                                        class="text-primary focus:ring-primary h-4 w-4 border-gray-300"
-                                    />
-                                </div>
-
-                                <label
-                                    for="tabby"
-                                    class="flex items-center gap-x-2 text-sm font-medium leading-6 text-gray-900"
-                                >
-                                    <img
-                                        class="h-5 w-auto"
-                                        src="{{ asset("storage/icons/tabby.svg") }}"
-                                        alt=""
-                                    />
-                                    <p>
-                                        {{ __("store.Pay in 4. No interest, no fees") }}
-                                    </p>
-                                </label>
-                            </div>
-                        @endif
-
-                        <div
-                            x-cloak
-                            x-data="{ show: false }"
-                            x-effect="show = $wire.form.payment_method === 'tabby'"
-                        >
-                            <div
-                                x-show="show"
-                                x-collapse.duration.800ms
-                                class="mt-4 border-t border-gray-200 p-4"
-                            >
-                                <x-checkout.tabby-payment
-                                    :total="$total"
-                                    :is-available="$isAvailable"
+                <div>
+                    @if ($isAvailable)
+                        <div class="relative flex gap-x-4 pb-4">
+                            <div class="flex h-6 items-center">
+                                <input
+                                    id="tabby"
+                                    name="payment_method"
+                                    type="radio"
+                                    wire:model.live="form.payment_method"
+                                    value="{{ \App\Enums\Checkout\PaymentMethod::TABBY->value }}"
+                                    class="text-primary focus:ring-primary h-4 w-4 border-gray-300"
                                 />
                             </div>
+
+                            <label
+                                for="tabby"
+                                class="flex items-center gap-x-2 text-sm font-medium leading-6 text-gray-900"
+                            >
+                                <img
+                                    class="h-5 w-auto"
+                                    src="{{ asset("storage/icons/tabby.svg") }}"
+                                    alt=""
+                                />
+                                <p>
+                                    {{ __("store.Pay in 4. No interest, no fees") }}
+                                </p>
+                            </label>
                         </div>
-                        @if ($rejectionReason)
-                            <div class="mt-4 rounded-lg bg-red-50 p-4">
-                                <div class="text-sm text-red-700">
-                                    @if ($rejectionReason === "order_amount_too_high")
-                                        {{ __("store.This purchase is above your current spending limit with Tabby, try a smaller cart or use another payment method") }}
-                                    @elseif ($rejectionReason === "order_amount_too_low")
-                                        {{ __("store.The purchase amount is below the minimum amount required to use Tabby, try adding more items or use another payment method") }}
-                                    @elseif ($rejectionReason === "cancelled_by_user")
-                                        {{ __("store.You aborted the payment") }}
-                                    @else
-                                        {{ __("store.Sorry, Tabby is unable to approve this purchase. Please use an alternative payment method for your order") }}
-                                    @endif
-                                </div>
-                            </div>
-                        @endif
+                    @endif
+
+                    <div
+                        x-cloak
+                        x-data="{ show: false }"
+                        x-effect="show = $wire.form.payment_method === 'tabby'"
+                    >
+                        <div
+                            x-show="show"
+                            x-collapse.duration.800ms
+                            class="mt-4 border-t border-gray-200 p-4"
+                        >
+                            <x-checkout.tabby-payment
+                                :total="$total"
+                                :is-available="$isAvailable"
+                            />
+                        </div>
                     </div>
-                @endif
+                    @if ($rejectionReason)
+                        <div class="mt-4 rounded-lg bg-red-50 p-4">
+                            <div class="text-sm text-red-700">
+                                @if ($rejectionReason === "order_amount_too_high")
+                                    {{ __("store.This purchase is above your current spending limit with Tabby, try a smaller cart or use another payment method") }}
+                                @elseif ($rejectionReason === "order_amount_too_low")
+                                    {{ __("store.The purchase amount is below the minimum amount required to use Tabby, try adding more items or use another payment method") }}
+                                @elseif ($rejectionReason === "cancelled_by_user")
+                                    {{ __("store.You aborted the payment") }}
+                                @else
+                                    {{ __("store.Sorry, Tabby is unable to approve this purchase. Please use an alternative payment method for your order") }}
+                                @endif
+                            </div>
+                        </div>
+                    @endif
+                </div>
             </div>
         </div>
 
