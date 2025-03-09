d03d977c17b39338fb47f91d3001597e0bfe621a
naram <naramalkoht@gmail.com>
2025-03-09 15:52:50 +0300

    Post Category

--- resources/views/components/blog/category-label.blade.php
+++ resources/views/components/blog/category-label.blade.php
@@ -2,7 +2,6 @@
     "post",
 ])
 @if (count($post->categories))
-    {{-- TODO: add url to archive page --}}
     <a
         href="{{ route("posts.index", ["categoryId" => $post->categories->first()->id]) }}"
         {{ $attributes->class(["inline-block w-fit rounded-3xl bg-secondaryColor px-6 py-2 text-xs"]) }}
