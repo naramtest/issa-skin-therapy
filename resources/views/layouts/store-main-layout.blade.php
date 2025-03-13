f27520bb237aaf57a1c291368b20763dcf805460
naram <naramalkoht@gmail.com>
2025-03-13 11:28:49 +0300

    google analytics

--- resources/views/layouts/store-main-layout.blade.php
+++ resources/views/layouts/store-main-layout.blade.php
@@ -7,6 +7,25 @@
     dir="{{ app()->getLocale() == "en" ? "ltr" : "rtl" }}"
 >
     <head>
+        <!-- Google tag (gtag.js) -->
+        @production
+            <script
+                async
+                src="https://www.googletagmanager.com/gtag/js?id=G-XNMQWTH0XH"
+            ></script>
+            <script>
+                window.dataLayer = window.dataLayer || [];
+
+                function gtag() {
+                    dataLayer.push(arguments);
+                }
+
+                gtag('js', new Date());
+
+                gtag('config', 'G-XNMQWTH0XH');
+            </script>
+        @endproduction
+
         <meta charset="utf-8" />
         <meta
             name="viewport"
